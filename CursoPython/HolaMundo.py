441cf4e3d9c7794d1e7cdb55d44ff446a9ec660d
alondrapj <105828037+alondrapj@users.noreply.github.com>
2022-05-18 19:50:54 -0500

    Primera subida de archivos

---
+++ CursoPython/HolaMundo.py
@@ -0,0 +1,10 @@
+# COMENTARIO: Saludo primer programa
+# Los comentarios nos sirven para documentar y describir 
+# cada parte del programa.
+# Los comentarios no los toma en cuenta o los omte Python
+print("Hola Mundo")
+print('Alondra Amira Palestina Jaime');
+print(5+5)
+print(10-2)
+print(8*3)
+print(20/4)
